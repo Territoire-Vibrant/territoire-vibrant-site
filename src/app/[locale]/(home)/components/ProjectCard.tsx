0bc25ab5b6b481b37d0a584638bed868424bd4a7
Pedro Lucena <40894497+PedroL22@users.noreply.github.com>
2025-11-11 01:09:16 -0300

    feat: Update ProjectCard colors + add secondary and tertiary theme colors

--- src/app/[locale]/(home)/components/ProjectCard.tsx
+++ src/app/[locale]/(home)/components/ProjectCard.tsx
@@ -11,11 +11,11 @@ export const ProjectCard = ({ image, title, description }: ProjectCardProps) => 
   return (
     <Link
       href=''
-      className='hover:-mt-5 group flex h-[25rem] w-72 select-none flex-col items-center justify-center rounded-2xl shadow-lg transition-all ease-in'
+      className='hover:-mt-5 group flex h-100 w-72 select-none flex-col items-center justify-center rounded-2xl shadow-lg transition-all ease-in'
     >
       <Image src={image} alt={title} className='h-[45%] w-full rounded-t-2xl bg-background object-cover' />
 
-      <div className='flex h-[65%] flex-col items-center space-y-2 rounded-b-2xl bg-neutral-200 p-6 text-center transition-all ease-in group-hover:bg-primary group-hover:text-background'>
+      <div className='flex h-[65%] flex-col items-center space-y-2 rounded-b-2xl bg-[#E0EBD5] p-6 text-center transition-all ease-in group-hover:bg-primary group-hover:text-background'>
         <div className='font-bold text-lg'>{title}</div>
 
         <div className='opacity-80'>{description}</div>
