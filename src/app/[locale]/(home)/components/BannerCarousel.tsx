d4fc006a1a812313128a1d7392a50280541f2973
Pedro Lucena <40894497+PedroL22@users.noreply.github.com>
2026-01-13 22:02:32 -0300

    chore: decrease "Portfolio" button size

--- src/app/[locale]/(home)/components/BannerCarousel.tsx
+++ src/app/[locale]/(home)/components/BannerCarousel.tsx
@@ -61,7 +61,7 @@ export function BannerCarousel({ slides }: { slides: BannerSlide[] }) {
         <Button
           type='button'
           variant='outline'
-          className='-translate-y-1/2 absolute top-full left-4 cursor-pointer transition-all ease-in lg:top-[96%] sm:top-[95%] hover:bg-primary hover:text-background'
+          className='-translate-y-1/2 absolute top-full left-4 h-fit cursor-pointer rounded-sm px-2 py-1.5 text-xs transition-all ease-in lg:top-[96%] sm:top-[95%] hover:bg-primary hover:text-background'
         >
           {t('portfolio')}
         </Button>
